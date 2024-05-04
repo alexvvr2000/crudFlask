266090ce5307d9c86905a98055bea0e574e314b4
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-04 16:35:23 -0600

    eliminado not null innecesario y datos prueba

--- crud/static/producto.sql
+++ crud/static/producto.sql
@@ -1,5 +1,10 @@
 create table Producto (
-    claveProducto int primary key auto_increment not null,
+    claveProducto int primary key auto_increment,
     descripcion varchar(255) not null,
     precio decimal(10,2) not null
 );
+INSERT INTO Producto (descripcion, precio) VALUES ('Producto A', 10.99);
+INSERT INTO Producto (descripcion, precio) VALUES ('Producto B', 20.50);
+INSERT INTO Producto (descripcion, precio) VALUES ('Producto C', 5.75);
+INSERT INTO Producto (descripcion, precio) VALUES ('Producto D', 15.25);
+INSERT INTO Producto (descripcion, precio) VALUES ('Producto E', 8.50);
